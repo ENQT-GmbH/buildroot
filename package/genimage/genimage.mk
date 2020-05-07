5533363fe6777feb1f9f33599f261a48a135fdc1
Sebastian Urban <surban@surban.net>
2020-05-07 17:45:25 +0200

    genimage update

--- package/genimage/genimage.mk
+++ package/genimage/genimage.mk
@@ -5,11 +5,11 @@
 ################################################################################
 
 GENIMAGE_VERSION = custom
-GENIMAGE_SOURCE = genimage-$(GENIMAGE_VERSION).tar.xz
 GENIMAGE_SITE = $(call github,pengutronix,genimage,2c139e93f9b6436da281fc8163f56bc8e8ff7a63)
 
 HOST_GENIMAGE_DEPENDENCIES = host-pkgconf host-libconfuse
 GENIMAGE_LICENSE = GPL-2.0
 GENIMAGE_LICENSE_FILES = COPYING
+GENIMAGE_AUTORECONF = YES
 
 $(eval $(host-autotools-package))
