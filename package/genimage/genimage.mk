f2e3155904fa6e94df41f459c1e76843874ec572
Sebastian Urban <surban@surban.net>
2020-05-07 17:31:13 +0200

    genimage temporary update

--- package/genimage/genimage.mk
+++ package/genimage/genimage.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-GENIMAGE_VERSION = 12
+GENIMAGE_VERSION = custom
 GENIMAGE_SOURCE = genimage-$(GENIMAGE_VERSION).tar.xz
-GENIMAGE_SITE = https://github.com/pengutronix/genimage/releases/download/v$(GENIMAGE_VERSION)
+GENIMAGE_SITE = $(call github,pengutronix,genimage,2c139e93f9b6436da281fc8163f56bc8e8ff7a63)
+
 HOST_GENIMAGE_DEPENDENCIES = host-pkgconf host-libconfuse
 GENIMAGE_LICENSE = GPL-2.0
 GENIMAGE_LICENSE_FILES = COPYING
